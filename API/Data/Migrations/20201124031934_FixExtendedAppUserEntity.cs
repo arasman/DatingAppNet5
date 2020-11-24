3fd508a7cdff8745e58086083cce60f51095f2e9
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2020-11-23 23:33:28 -0500

    End of section 8

---
+++ API/Data/Migrations/20201124031934_FixExtendedAppUserEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class FixExtendedAppUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lookingfor",
+                table: "Users",
+                newName: "LookingFor");
+
+            migrationBuilder.RenameColumn(
+                name: "KnowAs",
+                table: "Users",
+                newName: "KnownAs");
+
+            migrationBuilder.RenameColumn(
+                name: "County",
+                table: "Users",
+                newName: "Country");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LookingFor",
+                table: "Users",
+                newName: "Lookingfor");
+
+            migrationBuilder.RenameColumn(
+                name: "KnownAs",
+                table: "Users",
+                newName: "KnowAs");
+
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "Users",
+                newName: "County");
+        }
+    }
+}
